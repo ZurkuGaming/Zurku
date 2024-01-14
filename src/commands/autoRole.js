cc0760f1978e830f5af9ba19a863ae20ae99ed27
ZurkuGaming <zurkugaming@gmail.com>
2024-01-14 04:00:47 -0500

    zurku

--- src/commands/autoRole.js
+++ src/commands/autoRole.js
@@ -1,17 +1,20 @@
 // Import the GuildData model
 const GuildData = require('../models/guildData.js');
+const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: {
         name: 'autorole',
-        description: 'Set the role to be automatically assigned to new members.',
+        description: 'Configure your auto-role for this server.',
         category: 'Settings',
         options: [{
             name: 'role',
-            type: 8, // Role type
-            description: 'The role to assign to new members.',
+            type: ApplicationCommandOptionType.Role,
+            description: 'The role you want users to get on join.',
             required: true,
         }],
+        permissionsRequired: [PermissionFlagsBits.Administrator],
+        botPermissions: [PermissionFlagsBits.ManageRoles],
     },
     async execute(interaction) {
         // Check if the user has admin permissions
@@ -19,23 +22,25 @@ module.exports = {
             return interaction.reply('You do not have permission to use this command.');
         }
 
-        const role = interaction.options.get('role').role;
+        const targetRoleId = interaction.options.get('role').value;
 
         // Set the join role
-        const guildData = await GuildData.findOne({ guildID: interaction.guild.id });
+        let guildData = await GuildData.findOne({ guildID: interaction.guild.id });
         if (!guildData) {
             // If no data for the guild was found, create a new document
-            const newGuildData = new GuildData({
+            guildData = new GuildData({
                 guildID: interaction.guild.id,
-                joinRoleID: role.id,
+                joinRoleID: targetRoleId,
             });
-            await newGuildData.save();
         } else {
             // If data for the guild was found, update the document
-            guildData.joinRoleID = role.id;
-            await guildData.save();
+            if (guildData.joinRoleID === targetRoleId) {
+                return interaction.reply('Auto role has already been configured for that role. To disable run `/autorole-disable`');
+            }
+            guildData.joinRoleID = targetRoleId;
         }
 
-        await interaction.reply({ content: `Join role set to ${role.name}`, ephemeral: true });
+        await guildData.save();
+        await interaction.reply('Autorole has now been configured. To disable run `/autorole-disable`');
     },
 };
