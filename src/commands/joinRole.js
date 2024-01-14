6c93d327f48f53a33274c4271269a1d28dd4371a
ZurkuGaming <zurkugaming@gmail.com>
2024-01-14 03:30:33 -0500

    zurku

--- src/commands/joinRole.js
+++ src/commands/joinRole.js
@@ -1,28 +1,40 @@
-const GuildData = require('../models/guildData');
+// Import the GuildData model
+const GuildData = require('../models/guildData.js');
 
 module.exports = {
-    data: {
-        name: 'joinrole',
-        description: 'Set a role that is given to members when they join',
-    },
-    async execute(message, args) {
-        if (!message.member.hasPermission('ADMINISTRATOR')) {
-            return message.reply('You do not have the necessary permissions to use this command.');
-        }
+  data: {
+    name: 'joinrole',
+    description: 'Set the join role',
+    category: 'Settings',
+    options: [{
+      name: 'role',
+      description: 'The role to set as the join role',
+      type: 7,
+      required: true
+    }]
+  },
+  async execute (interaction) {
+    // Check if the user has admin permissions
+    if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+      return interaction.reply('You do not have permission to use this command.')
+    }
+    const role = interaction.options.get('role').role;
 
-        const role = message.mentions.roles.first();
-        if (!role) {
-            return message.reply(`No role mentioned.`);
-        }
+    // Set the goodbye role
+    const guildData = await GuildData.findOne({ guildID: interaction.guild.id });
+    if (!guildData) {
+      // If no data for the guild was found, create a new document
+      const newGuildData = new GuildData({
+        guildID: interaction.guild.id,
+        roleJoinID: role.id
+      });
+      await newGuildData.save();
+    } else {
+      // If data for the guild was found, update the document
+      guildData.roleJoinID = role.id;
+      await guildData.save();
+    }
 
-        let guildData = await GuildData.findOne({ guildID: message.guild.id });
-        if (!guildData) {
-            guildData = new GuildData({ guildID: message.guild.id });
-        }
-
-        guildData.joinRoleID = role.id;
-        await guildData.save();
-
-        return message.reply(`Join role has been set to ${role.name}`);
-    },
-};
+    await interaction.reply({ content: `Join role set to ${role.name}`, ephemeral: true });
+  }
+}
